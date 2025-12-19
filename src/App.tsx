9712d61fdc5ea9d9dd6c6cbea47ca05744c952f5
fuu-1 <moham18741@gmail.com>
2025-12-19 17:18:32 +0900

    feat : add Menu.ts

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,19 @@ import MainLayout from "./layout/MainLayout";
 import Sales from "./pages/sales/Sales";
 import Store from "./pages/store/Store";
 import MenuList from "./pages/menu/MenuList";
+import Login from "./pages/Login.tsx";
 
 function App() {
-  return (
-    <MainLayout>
-      <Routes>
-        <Route path="/sales" element={<Sales />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/menu" element={<MenuList />} />
-      </Routes>
-    </MainLayout>
-  );
+    return (
+        <MainLayout>
+            <Routes>
+                <Route path="/sales" element={<Sales />} />
+                <Route path="/store" element={<Store />} />
+                <Route path="/menu" element={<MenuList />} />
+                <Route path="/login" element={<Login/>} />
+            </Routes>
+        </MainLayout>
+    );
 }
 
 export default App;
---
+++ src/types/Menu.ts
@@ -0,0 +1,10 @@
+export interface Menu {
+    menuNo: number;
+    menuCategory: string;
+    menuCode: string;
+    menuName: string;
+    size: string;
+    menuPrice: number;
+    releaseStatus: string;
+    menuImage?: string;
+}
